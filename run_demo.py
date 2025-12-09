4c2f75d164b23527bf7d84eaeb00e88f06689572
synju <nuerotronic@gmail.com>
2025-12-09 13:45:25 +0200

    removed main.py .. old redundent demo launcher.. added run_demo.py to root.

--- run_demo.py
+++ run_demo.py
@@ -1,12 +1,11 @@
 """
-Example usage of Peach Engine
+Demo runner for Peach Engine
 """
-from peach_engine import PeachEngine
-from default_scene import DefaultScene
+from engine.peach_engine import PeachEngine
+from engine.default_scene import DefaultScene
 
 
 if __name__ == '__main__':
-    # Create engine
     engine = PeachEngine(
         width=1280,
         height=720,
@@ -14,11 +13,6 @@ if __name__ == '__main__':
         fps=60
     )
 
-    # Add scenes
     engine.scene_handler.add_scene('default', DefaultScene(engine))
-
-    # Set starting scene
     engine.scene_handler.set_scene('default')
-
-    # Run the engine
     engine.run()
