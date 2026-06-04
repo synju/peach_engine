e1087d1f6d80c275b449443281b8f6ef2bd52d4d
synju <nuerotronic@gmail.com>
2026-06-04 20:38:23 +0200

    feat:chunk generation

---
+++ area_43/maze_level/chunk.py
@@ -0,0 +1,225 @@
+from engine.mesh_object import MeshObject
+from panda3d.core import GeomVertexFormat, GeomVertexData, GeomVertexWriter
+from panda3d.core import Geom, GeomTriangles, GeomNode
+
+
+class Chunk:
+    SIZE = 16
+
+    def __init__(
+        self,
+        engine,
+        name="Chunk",
+        offset_x=0,
+        offset_y=0,
+        offset_z=0,
+        collision_enabled=False,
+    ):
+        self.engine = engine
+        self.name = name
+        self.offset = (offset_x, offset_y, offset_z)
+        self.voxels = [
+            [[0 for _ in range(self.SIZE)] for _ in range(self.SIZE)]
+            for _ in range(self.SIZE)
+        ]
+        self.mesh = MeshObject(engine, name)
+        self.mesh_node = None
+        self.collision_body = None
+        self.collision_np = None
+        self._collision_enabled = collision_enabled
+        self._build_mesh()
+
+    def set_voxel(self, x, y, z, voxel_type):
+        if 0 <= x < self.SIZE and 0 <= y < self.SIZE and 0 <= z < self.SIZE:
+            self.voxels[x][y][z] = voxel_type
+            self._build_mesh()
+
+    def get_voxel(self, x, y, z):
+        if 0 <= x < self.SIZE and 0 <= y < self.SIZE and 0 <= z < self.SIZE:
+            return self.voxels[x][y][z]
+        return 0
+
+    def _is_face_exposed(self, x, y, z, face):
+        nx, ny, nz = x, y, z
+        if face == 0:  # -X
+            nx -= 1
+        elif face == 1:  # +X
+            nx += 1
+        elif face == 2:  # -Y
+            ny -= 1
+        elif face == 3:  # +Y
+            ny += 1
+        elif face == 4:  # -Z (down)
+            nz -= 1
+        elif face == 5:  # +Z (up)
+            nz += 1
+
+        if (
+            nx < 0
+            or nx >= self.SIZE
+            or ny < 0
+            or ny >= self.SIZE
+            or nz < 0
+            or nz >= self.SIZE
+        ):
+            return True
+
+        return self.voxels[nx][ny][nz] == 0
+
+    def _build_mesh(self):
+        if self.mesh_node:
+            self.mesh_node.removeNode()
+
+        if self._collision_enabled and self.collision_np:
+            self.collision_np.removeNode()
+            self.collision_np = None
+            self.collision_body = None
+
+        format = GeomVertexFormat.get_v3n3c4()
+        vdata = GeomVertexData("chunk_verts", format, Geom.UHStatic)
+
+        vertex = GeomVertexWriter(vdata, "vertex")
+        normal = GeomVertexWriter(vdata, "normal")
+        color = GeomVertexWriter(vdata, "color")
+
+        tris = GeomTriangles(Geom.UHStatic)
+        vertex_index = 0
+
+        for x in range(self.SIZE):
+            for y in range(self.SIZE):
+                for z in range(self.SIZE):
+                    if self.voxels[x][y][z] == 0:
+                        continue
+
+                    world_x = x + self.offset[0]
+                    world_y = y + self.offset[1]
+                    world_z = z + self.offset[2]
+
+                    for face in range(6):
+                        if not self._is_face_exposed(x, y, z, face):
+                            continue
+
+                        self._add_face(
+                            vertex,
+                            normal,
+                            color,
+                            tris,
+                            world_x,
+                            world_y,
+                            world_z,
+                            face,
+                            vertex_index,
+                        )
+                        vertex_index += 4
+
+        if vertex_index == 0:
+            return
+
+        vdata.setNumRows(vertex_index)
+        geom = Geom(vdata)
+        geom.addPrimitive(tris)
+
+        node = GeomNode(self.name)
+        node.addGeom(geom)
+
+        self.mesh_node = self.mesh.node.attachNewNode(node)
+
+        if self._collision_enabled:
+            self.engine.utils.add_mesh_collider(self.mesh, self.engine.physics)
+
+    def _add_face(self, vertex, normal, color, tris, x, y, z, face, vertex_index):
+        h = 0.5
+
+        # Colors: Red, Green, Blue, Black, White, Grey
+        face_colors = [
+            (1.0, 0.0, 0.0, 1.0),  # face 0: Red (-X)
+            (0.0, 1.0, 0.0, 1.0),  # face 1: Green (+X)
+            (0.0, 0.0, 1.0, 1.0),  # face 2: Blue (-Y)
+            (0.0, 0.0, 0.0, 1.0),  # face 3: Black (+Y)
+            (1.0, 1.0, 1.0, 1.0),  # face 4: White (-Z)
+            (0.5, 0.5, 0.5, 1.0),  # face 5: Grey (+Z)
+        ]
+        fc = face_colors[face]
+
+        if face == 0:  # -X (left)
+            verts = [
+                (x - h, y - h, z - h),
+                (x - h, y + h, z - h),
+                (x - h, y + h, z + h),
+                (x - h, y - h, z + h),
+            ]
+            norms = [(-1, 0, 0)] * 4
+        elif face == 1:  # +X (right)
+            verts = [
+                (x + h, y - h, z + h),
+                (x + h, y + h, z + h),
+                (x + h, y + h, z - h),
+                (x + h, y - h, z - h),
+            ]
+            norms = [(1, 0, 0)] * 4
+        elif face == 2:  # -Y (back)
+            verts = [
+                (x + h, y - h, z - h),
+                (x + h, y - h, z + h),
+                (x - h, y - h, z + h),
+                (x - h, y - h, z - h),
+            ]
+            norms = [(0, -1, 0)] * 4
+        elif face == 3:  # +Y (front)
+            verts = [
+                (x - h, y + h, z - h),
+                (x - h, y + h, z + h),
+                (x + h, y + h, z + h),
+                (x + h, y + h, z - h),
+            ]
+            norms = [(0, 1, 0)] * 4
+        elif face == 4:  # -Z (bottom)
+            verts = [
+                (x - h, y - h, z - h),
+                (x + h, y - h, z - h),
+                (x + h, y + h, z - h),
+                (x - h, y + h, z - h),
+            ]
+            norms = [(0, 0, -1)] * 4
+        elif face == 5:  # +Z (top)
+            verts = [
+                (x - h, y - h, z + h),
+                (x - h, y + h, z + h),
+                (x + h, y + h, z + h),
+                (x + h, y - h, z + h),
+            ]
+            norms = [(0, 0, 1)] * 4
+
+        for i, v in enumerate(verts):
+            vertex.addData3(*v)
+            normal.addData3(*norms[i])
+            color.addData4(*fc)
+
+        # Flip winding order for Red, Green, Grey, White faces (0, 1, 4, 5)
+        if face in (0, 1, 4, 5):
+            tris.addVertices(vertex_index, vertex_index + 2, vertex_index + 1)
+            tris.addVertices(vertex_index, vertex_index + 3, vertex_index + 2)
+        else:
+            tris.addVertices(vertex_index, vertex_index + 1, vertex_index + 2)
+            tris.addVertices(vertex_index, vertex_index + 2, vertex_index + 3)
+        tris.closePrimitive()
+
+    def show(self):
+        if self.mesh_node:
+            self.mesh_node.show()
+
+    def hide(self):
+        if self.mesh_node:
+            self.mesh_node.hide()
+
+    def destroy(self):
+        if self.mesh_node:
+            self.mesh_node.removeNode()
+            self.mesh_node = None
+        if self.collision_np:
+            self.collision_np.removeNode()
+            self.collision_np = None
+        if self.collision_body:
+            self.engine.physics.removeRigidBody(self.collision_body)
+            self.collision_body = None
+        self.mesh.destroy()
--- area_43/scenes/maze.py
+++ area_43/scenes/maze.py
@@ -1,4 +1,5 @@
 from area_43.maze_level.maze import Maze
+from area_43.maze_level.chunk import Chunk
 from area_43.player import Player
 from area_43.free_flying_camera import FreeFlyingCamera
 from area_43.third_person_player import ThirdPersonPlayer
@@ -6,226 +7,275 @@ from engine.light import AmbientLight, DirectionalLight
 from engine.scene import Scene
 from engine.skybox import Skybox
 
+
 class MazeScene(Scene):
-	def __init__(self, engine):
-		super().__init__(engine, 'maze')
+    def __init__(self, engine):
+        super().__init__(engine, "maze")
 
-		# Disable Grid
-		self.engine.scene_handler.grid.hide()
+        # Disable Grid
+        self.engine.scene_handler.grid.hide()
 
-		# Skybox
-		self.skybox = None
+        # Skybox
+        self.skybox = None
 
-		# Lighting
-		self.ambient_light = None
-		self.sun_light = None
+        # Lighting
+        self.ambient_light = None
+        self.sun_light = None
 
-		# Player (first person)
-		self.player = None
+        # Player (first person)
+        self.player = None
 
-		# Third Person Player
-		self.third_person_player = None
-		self.use_third_person = False  # Start in third person
+        # Third Person Player
+        self.third_person_player = None
+        self.use_third_person = False  # Start in third person
 
-		# Free Flying Camera
-		self.free_cam = None
-		self.use_free_cam = False
+        # Free Flying Camera
+        self.free_cam = None
+        self.use_free_cam = False
 
-		# Level
-		self.maze = None
+        # Level
+        self.maze = None
+        self.chunk = None
 
-	def on_enter(self):
-		super().on_enter()
+    def on_enter(self):
+        super().on_enter()
 
-		# Skybox
-		self.setup_skybox()
+        # Skybox
+        self.setup_skybox()
 
-		# Lighting
-		self.setup_lights()
+        # Lighting
+        self.setup_lights()
 
-		# Sound
-		# self.setup_sound()
+        # Sound
+        # self.setup_sound()
 
-		# Players
-		self.setup_players()
+        # Players
+        self.setup_players()
 
-		# Cameras
-		self.setup_cameras()
+        # Cameras
+        self.setup_cameras()
 
-		# Level
-		self.setup_level()
+        # Level
+        self.setup_level()
 
-	def setup_skybox(self):
-		self.skybox = Skybox(self.engine, faces={
-			'right': ('assets/skydomes/sky_16_2k/sky_16_cubemap_2k/nx.png', 0, True),
-			'left': ('assets/skydomes/sky_16_2k/sky_16_cubemap_2k/px.png', 0, True),
-			'top': ('assets/skydomes/sky_16_2k/sky_16_cubemap_2k/py.png', 0, True),
-			'bottom': ('assets/skydomes/sky_16_2k/sky_16_cubemap_2k/ny.png', 0, False),
-			'front': ('assets/skydomes/sky_16_2k/sky_16_cubemap_2k/nz.png', 0, False),
-			'back': ('assets/skydomes/sky_16_2k/sky_16_cubemap_2k/pz.png', 0, False),
-		})
+    def setup_skybox(self):
+        self.skybox = Skybox(
+            self.engine,
+            faces={
+                "right": (
+                    "assets/skydomes/sky_16_2k/sky_16_cubemap_2k/nx.png",
+                    0,
+                    True,
+                ),
+                "left": ("assets/skydomes/sky_16_2k/sky_16_cubemap_2k/px.png", 0, True),
+                "top": ("assets/skydomes/sky_16_2k/sky_16_cubemap_2k/py.png", 0, True),
+                "bottom": (
+                    "assets/skydomes/sky_16_2k/sky_16_cubemap_2k/ny.png",
+                    0,
+                    False,
+                ),
+                "front": (
+                    "assets/skydomes/sky_16_2k/sky_16_cubemap_2k/nz.png",
+                    0,
+                    False,
+                ),
+                "back": (
+                    "assets/skydomes/sky_16_2k/sky_16_cubemap_2k/pz.png",
+                    0,
+                    False,
+                ),
+            },
+        )
 
-	def setup_lights(self):
-		self.ambient_light = AmbientLight(self.engine, 'ambient', color=(0.3, 0.3, 0.3), light_enabled=True)
-		self.sun_light = DirectionalLight(self.engine, 'sun', color=(1, 1, 1), direction=(-1, 1, -1), position=(0, 0, 10), light_enabled=True)
+    def setup_lights(self):
+        self.ambient_light = AmbientLight(
+            self.engine, "ambient", color=(0.3, 0.3, 0.3), light_enabled=True
+        )
+        self.sun_light = DirectionalLight(
+            self.engine,
+            "sun",
+            color=(1, 1, 1),
+            direction=(-1, 1, -1),
+            position=(0, 0, 10),
+            light_enabled=True,
+        )
 
-	def setup_sound(self):
-		# Ambient sound
-		self.engine.sound_player.play('wind', 'assets/sounds/wind_000.mp3', loop=True, volume=0.2)
-		pass
+    def setup_sound(self):
+        # Ambient sound
+        self.engine.sound_player.play(
+            "wind", "assets/sounds/wind_000.mp3", loop=True, volume=0.2
+        )
+        pass
 
-	def setup_players(self):
-		# First Person Player
-		self.player = Player(
-			self.engine,
-			self.engine.physics,
-			position=(2, 2, 3.0),
-			rotation=(0, 0),
-			near_clip=0.01,
-			debug_mode=False
-		)
-		self.engine.renderer.set_camera(self.player.camera)
-		self.engine.input_handler.set_mouse_locked(True)
-		self.player.looking = True
+    def setup_players(self):
+        # First Person Player
+        self.player = Player(
+            self.engine,
+            self.engine.physics,
+            position=(2, 2, 3.0),
+            rotation=(0, 0),
+            near_clip=0.01,
+            debug_mode=False,
+        )
+        self.engine.renderer.set_camera(self.player.camera)
+        self.engine.input_handler.set_mouse_locked(True)
+        self.player.looking = True
 
-		# Third Person Player (different spawn location)
-		self.third_person_player = ThirdPersonPlayer(
-			self.engine,
-			self.engine.physics,
-			position=(0, 0, 1.0),
-			rotation=(0, 0),
-			near_clip=0.01,
-			debug_mode=True
-		)
+        # Third Person Player (different spawn location)
+        self.third_person_player = ThirdPersonPlayer(
+            self.engine,
+            self.engine.physics,
+            position=(0, 0, 1.0),
+            rotation=(0, 0),
+            near_clip=0.01,
+            debug_mode=True,
+        )
 
-	def setup_cameras(self):
-		# Set initial camera based on mode
-		if self.use_third_person:
-			self.engine.renderer.set_camera(self.third_person_player.camera)
-		else:
-			self.engine.renderer.set_camera(self.player.camera)
+    def setup_cameras(self):
+        # Set initial camera based on mode
+        if self.use_third_person:
+            self.engine.renderer.set_camera(self.third_person_player.camera)
+        else:
+            self.engine.renderer.set_camera(self.player.camera)
 
-		# Create free camera
-		self.free_cam = FreeFlyingCamera(self.engine, position=(0, -5, 3))
+        # Create free camera
+        self.free_cam = FreeFlyingCamera(self.engine, position=(0, -5, 3))
 
-	def setup_level(self):
-		self.maze = Maze(self.engine, size=15, cell_height=3, cell_thickness=1, seed=42)
-		self.maze.spawn(offset_x=0, offset_y=0)
+    def setup_level(self):
+        self.maze = Maze(self.engine, size=15, cell_height=3, cell_thickness=1, seed=42)
+        self.maze.spawn(offset_x=0, offset_y=0)
+        self.chunk = Chunk(
+            self.engine,
+            name="test_chunk",
+            offset_x=20,
+            offset_y=0,
+            offset_z=0,
+            collision_enabled=True,
+        )
+        for x in range(Chunk.SIZE):
+            for y in range(Chunk.SIZE):
+                for z in range(Chunk.SIZE):
+                    if z < 8:
+                        self.chunk.set_voxel(x, y, z, 1)
 
-	def switch_to_first_person(self):
-		"""Switch to first person player"""
-		self.use_third_person = False
-		self.engine.renderer.set_camera(self.player.camera)
-		self.engine.input_handler.set_mouse_locked(True)
-		self.player.looking = True
-		self.engine.scene_handler.console.print("Switched to First Person")
+    def switch_to_first_person(self):
+        """Switch to first person player"""
+        self.use_third_person = False
+        self.engine.renderer.set_camera(self.player.camera)
+        self.engine.input_handler.set_mouse_locked(True)
+        self.player.looking = True
+        self.engine.scene_handler.console.print("Switched to First Person")
 
-	def switch_to_third_person(self):
-		"""Switch to third person player"""
-		self.use_third_person = True
-		self.engine.renderer.set_camera(self.third_person_player.camera)
-		self.engine.input_handler.set_mouse_locked(True)
-		self.third_person_player.looking = True
-		self.engine.scene_handler.console.print("Switched to Third Person")
+    def switch_to_third_person(self):
+        """Switch to third person player"""
+        self.use_third_person = True
+        self.engine.renderer.set_camera(self.third_person_player.camera)
+        self.engine.input_handler.set_mouse_locked(True)
+        self.third_person_player.looking = True
+        self.engine.scene_handler.console.print("Switched to Third Person")
 
-	def handle_input(self, input_handler):
-		super().handle_input(input_handler)
+    def handle_input(self, input_handler):
+        super().handle_input(input_handler)
 
-		# Skip all input if console is open
-		if self.engine.scene_handler.console.is_open:
-			return
+        # Skip all input if console is open
+        if self.engine.scene_handler.console.is_open:
+            return
 
-		# Toggle between first and third person (F4)
-		if input_handler.is_key_down('f4'):
-			if self.use_third_person:
-				self.switch_to_first_person()
-			else:
-				self.switch_to_third_person()
-			return
+        # Toggle between first and third person (F4)
+        if input_handler.is_key_down("f4"):
+            if self.use_third_person:
+                self.switch_to_first_person()
+            else:
+                self.switch_to_third_person()
+            return
 
-		# Reset current player position
-		if input_handler.is_key_down('f5'):
-			if self.use_third_person:
-				self.third_person_player.reset()
-			else:
-				self.player.reset()
+        # Reset current player position
+        if input_handler.is_key_down("f5"):
+            if self.use_third_person:
+                self.third_person_player.reset()
+            else:
+                self.player.reset()
 
-		# Toggle Free Camera (Noclip)
-		if input_handler.is_key_down('n'):
-			# Flip use_free_cam
-			self.use_free_cam = not self.use_free_cam
+        # Toggle Free Camera (Noclip)
+        if input_handler.is_key_down("n"):
+            # Flip use_free_cam
+            self.use_free_cam = not self.use_free_cam
 
-			# Noclip ON
-			if self.use_free_cam:
-				# Position
-				if self.use_third_person:
-					self.free_cam.position = self.third_person_player.position
-				else:
-					self.free_cam.position = self.player.camera.position
+            # Noclip ON
+            if self.use_free_cam:
+                # Position
+                if self.use_third_person:
+                    self.free_cam.position = self.third_person_player.position
+                else:
+                    self.free_cam.position = self.player.camera.position
 
-				# Heading
-				self.free_cam.heading = self.player.heading
+                # Heading
+                self.free_cam.heading = self.player.heading
 
-				# Pitch
-				self.free_cam.pitch = self.player.pitch
+                # Pitch
+                self.free_cam.pitch = self.player.pitch
 
-			# Noclip OFF
-			if not self.use_free_cam:
-				# Position
-				if self.use_third_person:
-					self.third_person_player.position= self.free_cam.position
-				else:
-					pos = list(self.free_cam.position)
-					pos[2] -= self.player.eye_height
-					self.player._position = pos
+            # Noclip OFF
+            if not self.use_free_cam:
+                # Position
+                if self.use_third_person:
+                    self.third_person_player.position = self.free_cam.position
+                else:
+                    pos = list(self.free_cam.position)
+                    pos[2] -= self.player.eye_height
+                    self.player._position = pos
 
-				# Heading
-				self.player.heading = self.free_cam.heading
+                # Heading
+                self.player.heading = self.free_cam.heading
 
-				# Pitch
-				self.player.pitch = self.free_cam.pitch
+                # Pitch
+                self.player.pitch = self.free_cam.pitch
 
-			# Console logging
-			self.engine.scene_handler.console.print(f"Free cam: {'ON' if self.use_free_cam else 'OFF'}")
+            # Console logging
+            self.engine.scene_handler.console.print(
+                f"Free cam: {'ON' if self.use_free_cam else 'OFF'}"
+            )
 
-		# Send Input to active controller
-		if self.use_free_cam:
-			self.free_cam.handle_input(input_handler)
-		elif self.use_third_person:
-			self.third_person_player.handle_input(input_handler)
-		else:
-			self.player.handle_input(input_handler)
+        # Send Input to active controller
+        if self.use_free_cam:
+            self.free_cam.handle_input(input_handler)
+        elif self.use_third_person:
+            self.third_person_player.handle_input(input_handler)
+        else:
+            self.player.handle_input(input_handler)
 
-	def update(self, dt):
-		super().update(dt)
-		# Physics
-		self.engine.physics.doPhysics(dt)
+    def update(self, dt):
+        super().update(dt)
+        # Physics
+        self.engine.physics.doPhysics(dt)
 
-		# Player updates - Skip if console is open
-		if not self.engine.scene_handler.console.is_open:
-			if self.use_free_cam:
-				self.free_cam.update(dt)
-				# Keep active player's hitbox visible
-				if self.use_third_person:
-					self.third_person_player.update_debug_hitbox()
-				else:
-					self.player.update_debug_hitbox()
-			elif self.use_third_person:
-				self.third_person_player.update(dt)
-			else:
-				self.player.update(dt)
+        # Player updates - Skip if console is open
+        if not self.engine.scene_handler.console.is_open:
+            if self.use_free_cam:
+                self.free_cam.update(dt)
+                # Keep active player's hitbox visible
+                if self.use_third_person:
+                    self.third_person_player.update_debug_hitbox()
+                else:
+                    self.player.update_debug_hitbox()
+            elif self.use_third_person:
+                self.third_person_player.update(dt)
+            else:
+                self.player.update(dt)
 
-	def on_exit(self):
-		super().on_exit()
-		if self.ambient_light:
-			self.ambient_light.destroy()
-		if self.sun_light:
-			self.sun_light.destroy()
-		if self.skybox:
-			self.skybox.destroy()
-		if self.maze:
-			self.maze.destroy()
-		if self.player:
-			self.player.destroy()
-		if self.free_cam:
-			self.free_cam.destroy()
+    def on_exit(self):
+        super().on_exit()
+        if self.ambient_light:
+            self.ambient_light.destroy()
+        if self.sun_light:
+            self.sun_light.destroy()
+        if self.skybox:
+            self.skybox.destroy()
+        if self.maze:
+            self.maze.destroy()
+        if self.chunk:
+            self.chunk.destroy()
+        if self.player:
+            self.player.destroy()
+        if self.free_cam:
+            self.free_cam.destroy()
